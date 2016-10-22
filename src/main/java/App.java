31040e2255a98ad3cb485309e59fe55fcda439e5
Jean- Jacques <Jean- Jacques@JJTOSCH.ropp.jjnet>
2016-10-22 09:52:08 +0200

    Premier commit lo54

---
+++ src/main/java/App.java
@@ -0,0 +1,27 @@
+
+
+import fr.utbm.entity.Actor;
+import fr.utbm.tools.HibernateUtil;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+	public static void main(String[] args) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+                
+		// Sélection des températures
+		Query q = session.createQuery("from Actor");
+		List<Actor> actors = q.list();
+          
+		session.getTransaction().commit();
+		session.close();
+                System.exit(0);
+	}
+}
